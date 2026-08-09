2a6e60d4c8431e80a6d974dfac1cc6e6b46cb8e2
Pradip Nayak <pradipn962@gmail.com>
2026-08-09 08:23:13 +0530

    Loops - 1

--- src/Loops/newloop.java
+++ src/Loops/newloop.java
@@ -4,5 +4,10 @@ public class newloop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
+        int sum=0;
+        for(int i = 0;i<=a;i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
     }
 }
