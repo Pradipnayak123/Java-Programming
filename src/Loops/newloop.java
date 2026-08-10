f80cfc42aec14042b4bb0b320a7477920572ae9f
Pradip Nayak <pradipn962@gmail.com>
2026-08-10 18:44:26 +0530

    Loops - 1

--- src/Loops/newloop.java
+++ src/Loops/newloop.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class newloop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a :");
         int a = sc.nextInt();
         int sum=0;
         for(int i = 0;i<=a;i++){
