190fc326ad1c7fa6b137ad283ef22eca53ebc088
Pradip Nayak <pradipn962@gmail.com>
2026-08-16 23:50:14 +0530

    Add home Route

--- src/Loops/newloop.java
+++ src/Loops/newloop.java
@@ -4,7 +4,7 @@ public class newloop {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a :");
-        System.out.println("Hello java!...");
+        System.out.println("Hello java!...   this");
         int a = sc.nextInt();
         int sum=0;
         for(int i = 0;i<=a;i++){
