928ea1c2c9e8d0813c83173312938f35e9b8184d
Pradip Nayak <pradipn962@gmail.com>
2026-09-05 00:45:18 +0530

    Will Start Soon

--- src/newInput.java
+++ src/newInput.java
@@ -2,7 +2,7 @@ public class newInput {
     import java.util.scanner()
     public static void main(String[] args) {
         System.out.println("Heyy Java .c.h....");
-        System.out.println("Will  Start   Soon");
+        System.out.println("Will    Start   Soon");
 
 
     }
